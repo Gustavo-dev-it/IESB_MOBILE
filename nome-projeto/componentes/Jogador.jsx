eb3a895a599b337f3361351ed799ad075707ed69
Gustavo.Dev <tavintwd@gmail.com>
2025-04-04 21:36:29 -0300

    Exercicio

---
+++ nome-projeto/componentes/Jogador.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Avatar, Card } from 'react-native-paper';
+
+export default function Municipio(props) {
+
+  const { nome, numero, imagem } = props
+
+  return (
+    <Card.Title
+      title={nome}
+      subtitle={numero}
+      left={(props) => <Avatar.Image {...props} source={{ uri: imagem }} />}
+    />
+  )
+}
+
+const styles = StyleSheet.create({})
